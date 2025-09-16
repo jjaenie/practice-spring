b42b1dac118a57eb0e2f0095f35cdbfc3b8ab1e0
jjaenie <wogus990519@gmail.com>
2025-09-16 22:04:47 +0900

    memo

---
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -0,0 +1,54 @@
+package com.sparta.memo.controller;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.entity.Memo;
+import com.sparta.memo.service.MemoService;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MemoController {
+
+    private final MemoService memoService;
+
+    public MemoController(JdbcTemplate jdbcTemplate) {
+        this.memoService = new MemoService(jdbcTemplate);
+    }
+
+    @PostMapping("/memos")
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
+        return memoService.createMemo(requestDto);
+
+    }
+
+    @GetMapping("/memos")
+    public List<MemoResponseDto> getMemos() {
+
+        return memoService.getMemos();
+    }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+
+        return memoService.updateMemo(id, requestDto);
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+
+        return memoService.deleteMemo(id);
+    }
+
+
+}
---
+++ src/test/java/com/sparta/memo/MemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
