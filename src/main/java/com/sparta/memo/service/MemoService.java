cc63b08076f28325721b46fcf38f89220f81e311
jjaenie <wogus990519@gmail.com>
2025-09-16 23:01:05 +0900

    의존성 주입 완료

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,16 +4,33 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MemoService {
 
-    private final MemoRepository memoRepository;
+    // 1. 생성자 주입 <-- 생성자가 하나만 있을때는 @Autowired 생략 가능
+    private final MemoRepository memoRepository; //final은 바로 초기화를 하거나 생성자를 통해 초기화 해야함
 
     public MemoService(MemoRepository memoRepository) {
         this.memoRepository = memoRepository;
     }
+
+//    //2. 메서드로 주입
+//    private final MemoRepository memoRepository;
+//
+//    @Autowired
+//    public MemoService(MemoRepository memoRepository) {
+//        this.memoRepository = memoRepository;
+//    }
+
+//    //3. 필드로 주입 <-- 추천은 안함. 생성자 주입 추천
+//    @Autowired
+//    private MemoRepository memoRepository;
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
         // RequestDto -> Entity
